5aafb1dab1e8e51b6f6602fceed751abf99bab23
Arthur <58709410+Arthuthu@users.noreply.github.com>
2023-10-04 03:32:03 -0300

    Add projects routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import './css/site.css'
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./pages/Home"
+import Clinica from "./pages/Projects/Clinica"
+import NetCoreApp from "./pages/Projects/NetCoreApp"
+import Roadmap from "./pages/Projects/Roadmap"
+import OldPortfolio from "./pages/Projects/OldPortfolio"
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Layout />}>
-    <Route index element={<Home />}>
-      
-    </Route>
+    <Route index element={<Home />}/>
+    <Route path="clinica" element={<Clinica />}/>
+    <Route path="netcoreapp" element={<NetCoreApp />}/>
+    <Route path="roadmap" element={<Roadmap />}/>
+    <Route path="oldportfolio" element={<OldPortfolio />}/>
   </Route>
 ))
 
